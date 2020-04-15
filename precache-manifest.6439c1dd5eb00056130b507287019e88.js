2112fc3a80f179ee44ccdcbe6dbed6a0d86545c4
phil cowan <philipjcowan1@gmail.com>
2020-04-15 11:48:41 -0700

    Updates

---
+++ precache-manifest.6439c1dd5eb00056130b507287019e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63236b65f30cd604a08626d356596daf",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "cb74a61a48ec821d8002",
+    "url": "/clicky/static/css/main.d66a1aa4.chunk.css"
+  },
+  {
+    "revision": "6317c9b25b6891613dd1",
+    "url": "/clicky/static/js/2.242e5220.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky/static/js/2.242e5220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb74a61a48ec821d8002",
+    "url": "/clicky/static/js/main.125ed9ab.chunk.js"
+  },
+  {
+    "revision": "1ab451366b9ad674527a",
+    "url": "/clicky/static/js/runtime-main.b40413aa.js"
+  }
+]);
